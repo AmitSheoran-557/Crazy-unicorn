47d61e6bd07445d09dc2713d2548d7fca1c5b06a
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2025-01-15 11:17:25 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Hero from './components/Hero';
+import Team from './components/Team';
 
 function App() {
   return (
     <>
-    <Hero/>
+    <Team/>
     </> 
   );
 }
---
+++ src/utils/helper.js
@@ -0,0 +1,14 @@
+export const TEAM_CARDS = [
+    {
+        heading: "Jerome Bell",
+        description: "(CEO)"
+    },
+    {
+        heading: "Jerome Bell",
+        description: "(CEO)"
+    },
+    {
+        heading: "Jerome Bell",
+        description: "(CEO)"
+    }
+];
