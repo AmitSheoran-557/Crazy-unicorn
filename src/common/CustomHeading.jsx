d0fe4c9dae5076a6596cd0f996f8cf2de6627fd5
AmitSheoran-557 <amit22082006@gmail.com>
2025-01-15 12:41:49 +0530

    add heading

--- src/common/CustomHeading.jsx
+++ src/common/CustomHeading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CustomHeading = (props) => {
   return (
-    <h2 className={`xl:text-[70px] lg:text-6xl md:text-5xl text-4xl !leading-[115%] ${props.className}`}>{props.title}</h2>
+    <h2 className={`xl:text-[70px] lg:text-6xl md:text-5xl text-4xl !leading-[115%] text-black ${props.className}`}>{props.title}</h2>
   )
 }
 
--- src/components/OurStory.jsx
+++ src/components/OurStory.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import CustomHeading from '../common/CustomHeading'
 
 const OurStory = () => {
   return (
     <div className='max-w-[1320px] mx-auto flex flex-col justify-center items-center'>
+      <CustomHeading title="Our Story" className="lg:mb-[22px] md:mb-4 mb-3"/>
     </div>
   )
 }
