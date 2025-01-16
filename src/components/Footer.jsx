c426ac14f261fa0b1db320e64a807d16e418a549
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2025-01-16 14:35:57 +0530

    border

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,32 +16,29 @@ function Footer() {
         email: ''
     });
 
-    // Create a ref for the form
     const formRef = useRef();
 
     const handleSubmit = e => {
         e.preventDefault();
 
-        // Ensure formRef is available and pass it to emailjs.sendForm
         emailjs
           .sendForm('service_vubs3cl', 'template_lb5dsje', formRef.current, {
             publicKey: 'Psw6rvjLDppWVhvBu',
           })
           .then(
             (result) => {
-              // Success callback
+
               Swal.fire({
                 title: 'Success',
                 text: 'Your email has been sent successfully',
                 icon: 'success',
                 confirmButtonText: 'Thanks',
               }).then(() => {
-                setFormValue({ email: '' }); // Reset the form value after success
+                setFormValue({ email: '' });
               });
               console.log('SUCCESS!', result);
             },
             (error) => {
-              // Error callback
               Swal.fire({
                 title: 'Error',
                 text: 'Something went wrong. Please try again later.',
@@ -52,7 +49,6 @@ function Footer() {
             }
           );
 
-        // Validation for empty email input
         if (formValue.email === '') {
             alert('Please enter a valid email address.');
             return;
@@ -73,16 +69,17 @@ function Footer() {
                 alt="footer clouds"
             />
             <img
-                className='pointer-events-none absolute bottom-40 max-xl:bottom-52 max-lg:bottom-32 max-md:bottom-0 max-md:max-w-[180px] max-lg:max-w-[250px] right-0 w-full max-w-[400px] max-xl:max-w-[300px] z-[1] animate-rainbow'
+                className='pointer-events-none absolute bottom-40 max-xl:bottom-52 max-lg:bottom-32 max-md:bottom-0 max-md:max-w-[180px] max-lg:max-w-[250px] right-0 w-full xl:max-[800px] max-w-[400px] max-xl:max-w-[300px] z-[1] animate-rainbow'
                 src={Rainbow}
                 alt="rainbow"
             />
+            <div className='relative z-[3]'>
             <CustomHeading title="Sign up for the latest news" className="text-center" />
             <p className='text-center max-w-[660px] mx-auto text-custom-xl max-md:text-base leading-[170%] font-comic'>
                 Convallis et vel cras odio mi, volutpat sed ultrices. Sed amet sed aenean egestas ut sit.
             </p>
             <form
-                ref={formRef} // Attach the formRef here
+                ref={formRef}
                 onSubmit={handleSubmit}
                 action='#'
                 className='bg-white max-w-[521px] text-black placeholder:text-black placeholder:opacity-70 max-sm:max-w-[330px] mx-auto mt-[50px] max-lg:mt-11 max-md:mt-8 max-sm:mt-5 py-2 pl-[19px] pr-[9px] rounded-[10px] w-full flex items-center'
@@ -96,12 +93,12 @@ function Footer() {
                     name='email'
                     type='email'
                     placeholder='Your email'
-                    className='px-[9px] w-full py-[15px] outline-none'
+                    className='px-[9px] font-comic placeholder:font-comic w-full py-[15px] outline-none'
                 />
-                <div className="border-linear-gradient max-w-max rounded-xl ml-2 mt-2 max-xl:mx-auto">
+                <div className="border-linear-gradient max-w-max rounded-xl mt-2 max-xl:mx-auto">
                     <div className='bg-white md:-translate-x-2 -translate-x-1 md:-translate-y-2 -translate-y-1 rounded-xl'>
                     <CustomButton
-                        className="uppercase py-[10px] px-[29px] bg-clip-text text-transparent bg-gradient-to-r from-[#B62AFF] to-[#FF0DF2]"
+                        className="uppercase py-[10px] px-[29px] !text-2xl bg-clip-text text-transparent bg-gradient-to-r from-[#B62AFF] to-[#FF0DF2]"
                         title='Subscribe'
                         type="submit"
                         disabled={formValue.email === ''}
@@ -109,6 +106,7 @@ function Footer() {
                     </div>
                 </div>
             </form>
+            </div>
             <div className='mt-[287px] max-xl:mt-40 max-lg:mt-24 max-md:mt-16 max-sm:mt-10 relative z-10'>
                 <img className='max-w-[172px] max-md:max-w-[130px] mx-auto bg-transparent' src={FooterLogo} alt="footer logo" />
                 <div className='flex justify-center gap-5 mt-[60px] max-md:mt-12 max-sm:mt-5'>
