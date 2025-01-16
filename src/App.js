ef050eb0e585448a099d15a06069a70e26032af9
AmitSheoran-557 <amit22082006@gmail.com>
2025-01-16 15:05:34 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import AboutUs from './components/AboutUs';
 import Faq from './components/Faq';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import OurStory from './components/OurStory';
+import Roadmap from './components/Roadmap';
 import Team from './components/Team';
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
     <>
     <Hero/>
     <OurStory/>
+    <AboutUs/>
+    <Roadmap/>
     <Team/>
     <Faq/>
     <Footer/>
