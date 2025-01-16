894cc4507c7a329329311cb10c8ead018be3241b
AmitSheoran-557 <amit22082006@gmail.com>
2025-01-16 15:14:23 +0530

    update

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,28 +1,101 @@
 import { DiscordLogo, InstagramLogo, TwitterLogo } from "./icons";
 export const TEAM_CARDS = [
+    {
+        heading: "Jerome Bell",
+        description: "(CEO)"
+    },
+    {
+        heading: "Jenny Wilson",
+        description: "(Founder)"
+    },
+    {
+        heading: "Ralph Edwards",
+        description: "(Team Member)"
+    }
+];
+export const HEADER_LIST = [
   {
-    title: "Home",
-    link: "#home",
+      title: "Home",
+      link: "#home",
   },
   {
-    title: "Story",
-    link: "#story",
+      title: "Story",
+      link: "#story",
   },
   {
-    title: "About",
-    link: "#about",
+      title: "About",
+      link: "#about",
   },
   {
-    title: "Roadmap",
-    link: "#roadmap",
+      title: "Roadmap",
+      link: "#roadmap",
   },
   {
-    title: "Team",
-    link: "#team",
+      title: "Team",
+      link: "#team",
   },
   {
-    title: "FAQs",
-    link: "#faq",
+      title: "FAQs",
+      link: "#faq",
   },
 ];
->>>>>>> c6d9c47a12ad41bd028ec13eff923e5eefbec17d
+
+export const FAQ_LIST = [
+    {
+      heading: "Q.1 Eu faucibus lorem nulla cras ?",
+      subHeading:
+        "Pulvinar odio scelerisque suspendisse fringilla rutrum a. Vestibulum etiam egestas dictum tristique. Eu quis in eu nunc, aliquet in non elit. Dis tincidunt.",
+    },
+  
+    {
+      heading: "Q.2 Eu adipiscing ac felis mauris ?",
+      subHeading:
+        "Pulvinar odio scelerisque suspendisse fringilla rutrum a. Vestibulum etiam egestas dictum tristique. Eu quis in eu nunc, aliquet in non elit. Dis tincidunt.",
+    },
+  
+    {
+      heading:
+        "Q.3 Ridiculus amet gravida accumsan dis ?",
+      subHeading:
+        "Pulvinar odio scelerisque suspendisse fringilla rutrum a. Vestibulum etiam egestas dictum tristique. Eu quis in eu nunc, aliquet in non elit. Dis tincidunt.",
+    },
+  
+    {
+      heading:
+        "Q.4 Magna eu diam nec quis turpis tortor pulvinar ?",
+      subHeading:
+        "Pulvinar odio scelerisque suspendisse fringilla rutrum a. Vestibulum etiam egestas dictum tristique. Eu quis in eu nunc, aliquet in non elit. Dis tincidunt.",
+    },
+  
+    {
+      heading: "Q.5 Morbi at cursus ut in augue ?",
+      subHeading:
+        "Pulvinar odio scelerisque suspendisse fringilla rutrum a. Vestibulum etiam egestas dictum tristique. Eu quis in eu nunc, aliquet in non elit. Dis tincidunt.",
+    },
+  
+    {
+      heading: "Q.6 Cras pellentesque convallis semper dolor at vel ?",
+      subHeading:
+        "Pulvinar odio scelerisque suspendisse fringilla rutrum a. Vestibulum etiam egestas dictum tristique. Eu quis in eu nunc, aliquet in non elit. Dis tincidunt.",
+    },
+    {
+        heading: "Q.7 gravida maecenas amet tincidunt feugiat ?",
+        subHeading:
+          "Pulvinar odio scelerisque suspendisse fringilla rutrum a. Vestibulum etiam egestas dictum tristique. Eu quis in eu nunc, aliquet in non elit. Dis tincidunt.",
+      }
+  ];
+
+  export const MEDIA_ICONS = [
+    {
+        icon: <TwitterLogo/>,
+        link: "https://twitter.com/"
+    },
+    {
+        icon: <InstagramLogo/>,
+        link: "https://www.instagram.com/"
+    },
+    {
+        icon: <DiscordLogo/>,
+        link: "https://discord.com/"
+    }
+  ]
